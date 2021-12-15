6bbf274eb3b878568eda24c613f59da73d15cc44
Sandeep Sharma <95605363+sandeep-lby@users.noreply.github.com>
2021-12-16 00:47:19 +1300

    Basic .NET6 API for items with CRUD and MongoDB

---
+++ Play.Catalog.Service/Settings/MongoDBSettings.cs
@@ -0,0 +1,12 @@
+namespace Play.Catalog.Service.Settings
+{
+    public class MongoDBSettings
+    {
+        public string Host { get; init; }
+
+        public int Port { get; init; }
+
+        public string ConnectionString => $"mongodb://{Host}:{Port}";
+        //public string ConnectionString => $"mongodb://localhost:27017";
+    }
+}
